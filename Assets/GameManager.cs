de09393074b8ee6e934600b6e04194b87735cca4
kksumm123 <kksumm123@gmail.com>
2021-10-14 17:20:51 +0900

    1. 무빙큐브 프리팹으로 2. GameManager 추가

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject movingCube;
+    [SerializeField] float positionValue = 2;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/MovingCube.cs
+++ Assets/MovingCube.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
-    [SerializeField] float posValue = 2;
     Vector3 desPos;
     Vector3 startPos;
     float startTime;
@@ -15,9 +14,8 @@ public class MovingCube : MonoBehaviour
         cubeLayer = 1 << LayerMask.NameToLayer("Cube");
         Physics.Raycast(new Vector3(0, 1000, 0), Vector3.down, out hitInfo, 1000, cubeLayer);
         
-        var randValue = Random.Range(0, 2) == 0 ? 1 : -1f;
         var posY = 0.5f * transform.localScale.y + hitInfo.point.y;
-        startPos = new Vector3(posValue * randValue, posY, posValue);
+        startPos = transform.position;
         desPos = startPos;
         desPos.x *= -1f;
         desPos.z *= -1f;
