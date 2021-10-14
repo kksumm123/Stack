7fb6fb40428db31df6b4f57c7613e3f85f0a12af
kksumm123 <kksumm123@gmail.com>
2021-10-14 17:17:23 +0900

    1. MovingCube.cs 작성완료 2. Cube에 Cube레이어 추가

--- Assets/MovingCube.cs
+++ Assets/MovingCube.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class MovingCube : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float posValue = 2;
+    Vector3 desPos;
+    Vector3 startPos;
+    float startTime;
+    RaycastHit hitInfo;
+    LayerMask cubeLayer;
     void Start()
     {
+        cubeLayer = 1 << LayerMask.NameToLayer("Cube");
+        Physics.Raycast(new Vector3(0, 1000, 0), Vector3.down, out hitInfo, 1000, cubeLayer);
         
+        var randValue = Random.Range(0, 2) == 0 ? 1 : -1f;
+        var posY = 0.5f * transform.localScale.y + hitInfo.point.y;
+        startPos = new Vector3(posValue * randValue, posY, posValue);
+        desPos = startPos;
+        desPos.x *= -1f;
+        desPos.z *= -1f;
+        startTime = Time.time;
     }
 
-    // Update is called once per frame
+    float elapsedTime;
+    float calcTimeResult;
+    Vector3 pos;
     void Update()
     {
-        
+        elapsedTime = Time.time - startTime;
+        calcTimeResult = Mathf.Abs(elapsedTime % 2 - 1f);
+        pos = Vector3.Lerp(desPos, startPos, calcTimeResult);
+        transform.position = pos;
     }
 }
