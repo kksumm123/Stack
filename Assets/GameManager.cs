c9ab039c21a5412d7cb054490c6d894bedfc79ae
kksumm123 <kksumm123@gmail.com>
2021-10-14 18:08:28 +0900

    1. 큐브 색깔 변경 2. 카메라 위로 올리는 기능 완료

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject movingCube;
+    public const string movingCubeStr = "MovingCube";
+    GameObject movingCube;
     [SerializeField] float cubeHeight = 0.2f;
     [SerializeField] float positionValue = 2;
+    Color cubeColor = new Color(214, 255, 107, 255);
+    [SerializeField] float colorStep = 5f;
     void Start()
     {
+        movingCube = (GameObject)Resources.Load(movingCubeStr);
+        cubeColor = movingCube.GetComponent<Renderer>().sharedMaterial
+                              .GetColor("_BaseColor");
         CreateCube();
     }
 
@@ -23,9 +29,15 @@ public class GameManager : MonoBehaviour
     private void CreateCube()
     {
         cubeCount++;
-        var direction = cubeCount % 2 == 1 ? 1 : -1f;
-        Instantiate(movingCube,
-                    new Vector3(positionValue * direction, cubeCount * cubeHeight, positionValue),
-                    Quaternion.Euler(0, 45, 0));
+        var dir = cubeCount % 2 == 1 ? 1 : -1f;
+        var newGo = Instantiate(movingCube,
+                        new Vector3(positionValue * dir, cubeCount * cubeHeight, positionValue),
+                        Quaternion.Euler(0, 45, 0));
+        Color.RGBToHSV(cubeColor, out float h, out float s, out float v);
+        cubeColor = Color.HSVToRGB(h + (1f / 256) * colorStep, s, v);
+        newGo.GetComponent<Renderer>().material.SetColor("_BaseColor", cubeColor);
+
+        // 카메라 위로 이동
+        Camera.main.transform.Translate(0, cubeHeight, 0, Space.World);
     }
 }
