733e044e8bfb9aae4c31a2659a01c9cd42f0c447
kksumm123 <kksumm123@gmail.com>
2021-10-14 17:42:40 +0900

    1. 레이캐스트방식 삭제, Height 계산하도록 2. Cube, Fog, BG, Cam Y값 -0.4f

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject movingCube;
+    [SerializeField] float cubeHeight = 0.2f;
     [SerializeField] float positionValue = 2;
     void Start()
     {
-        
+        CreateCube();
     }
 
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+            CreateCube();
+    }
+
+    [SerializeField] int cubeCount = 0;
+    private void CreateCube()
+    {
+        cubeCount++;
+        var direction = cubeCount % 2 == 1 ? 1 : -1f;
+        Instantiate(movingCube,
+                    new Vector3(positionValue * direction, cubeCount * cubeHeight, positionValue),
+                    Quaternion.Euler(0, 45, 0));
     }
 }
--- Assets/MovingCube.cs
+++ Assets/MovingCube.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,8 @@ public class MovingCube : MonoBehaviour
     Vector3 desPos;
     Vector3 startPos;
     float startTime;
-    RaycastHit hitInfo;
-    LayerMask cubeLayer;
     void Start()
     {
-        cubeLayer = 1 << LayerMask.NameToLayer("Cube");
-        Physics.Raycast(new Vector3(0, 1000, 0), Vector3.down, out hitInfo, 1000, cubeLayer);
-        
-        var posY = 0.5f * transform.localScale.y + hitInfo.point.y;
         startPos = transform.position;
         desPos = startPos;
         desPos.x *= -1f;
@@ -26,6 +21,18 @@ public class MovingCube : MonoBehaviour
     float calcTimeResult;
     Vector3 pos;
     void Update()
+    {
+        Move();
+        Stop();
+    }
+
+    private void Stop()
+    {
+        if (Input.anyKeyDown)
+            enabled = false;
+    }
+
+    private void Move()
     {
         elapsedTime = Time.time - startTime;
         calcTimeResult = Mathf.Abs(elapsedTime % 2 - 1f);
